0d80f7a68d34a92a331f794a2cf7d99ed8647cc9
tran <fidtran@gmail.com>
2020-08-27 02:02:09 -0400

    fix to deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	apiBaseUrl:
-		process.env.NODE_ENV === "development"
+	baseUrl:
+		process.env.NODE_ENV == "development"
 			? process.env.REACT_APP_API_BASE_URL
 			: "https://biblur-back.herokuapp.com",
 };
